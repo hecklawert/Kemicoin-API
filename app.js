a36c27ad7d8f2801f0ddf2ffd4201738c9b416a4
Hëck Lawert <hectorgutifer@gmail.com>
2021-03-25 23:11:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+const mongoose = require('mongoose')
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var cors = require('cors')
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
+// Bring in the Database Config
+const db = require('./config/keys').mongoURI
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  authSource: "admin"
+}).then(() => {
+  console.log(`Database connected succesfully ${db}`)
+}).catch(err => {
+  console.log(`Unable to connect with the database ${err}`)
+})
+
+// Router
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404).send('Unable to find the requested resource!');
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+});
+
+app.options('*', cors())
+
+module.exports = app;
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb://root:kemicoin@localhost:27017/kemicoin", // If you are seeing this, yeah, I know... hardcoded credentials is not a good idea
+    secret: "mysecret"
+}
